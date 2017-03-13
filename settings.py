fcce545ea5b0176ae0109ff7abb115d4e81fae39
jamsiedaly <14152789@studentmail.ul.ie>
2017-03-13 23:15:22 +0000

    Made speed scale
    travels pixels per second relative to the resolution

--- settings.py
+++ settings.py
@@ -9,12 +9,12 @@ class Settings():
 		self.screen_height = self.info.current_h
 		pygame.display.set_mode((self.screen_width,self.screen_height),pygame.FULLSCREEN)
 		self.bg_color = (0, 0, 20)
-		self.ship_speed_factor = 7.5
-		self.star_speed = 1
-		self.bullet_speed_factor =10
+		self.ship_speed_factor = self.screen_width/140
+		self.star_speed = self.screen_width/300
+		self.bullet_speed_factor = self.screen_width/50
 		self.bullet_width = 3
 		self.bullet_height = 15
 		self.bullet_color = 150, 150, 255
 		self.time = 100
-		self.alien_speed = 5
+		self.alien_speed = self.screen_width/200
 		self.alien_frequency = 100
