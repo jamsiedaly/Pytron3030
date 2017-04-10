84451a51209fb57bf07ab220ac557c38a75a5773
jamsiedaly <14152789@studentmail.ul.ie>
2017-04-10 22:29:40 +0100

    fixed bug

--- timer.py
+++ timer.py
@@ -14,7 +14,7 @@ class Timer():
 	def update(self, delay):
 		if self.time >= 0:
 			self.time -= delay
-			self.color = (abs(255*(self.time/self.default)),255 - abs(255*(self.time/self.default)), 0)
+			self.color = (abs(self.time/self.default),abs(self.time/self.default), 0)
 			self.rect = (100, 700, 25, -0.2*self.time)
 		else:
 			self.rect = (100, 700, 25, -200)
@@ -23,9 +23,8 @@ class Timer():
 		self.time = self.default
 		
 	def erase(self):
-		pygame.draw.rect(self.screen, self.bg_color, (100, 720, 25, -220))
+		pygame.draw.rect(self.screen, self.bg_color, (100, 720, 25, -200))
 		
 	def blitme(self):
-		print(self.color)
 		print(self.time/self.default)
 		pygame.draw.rect(self.screen, self.color, self.rect)
